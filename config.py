72b6a14f28213ede4989a2985fcc7b4d4d9bb6c7
NM-bl@cker <87934966+nadunmalaka@users.noreply.github.com>
2021-08-23 11:28:47 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #Uvindu Bro <https://t.me/UvinduBro>
 
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("7249950"))
+API_HASH = os.getenv("7c5948ab2da557a035e48e9d5f652cb5")
+BOT_TOKEN = os.getenv("1920922257:AAFbvMiJRuCeYp-Q4l4zyWBt-grgekHbFrI")
