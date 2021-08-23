46cf054617a24b175044b0cf76de02959a0f9bee
NM-bl@cker <87934966+nadunmalaka@users.noreply.github.com>
2021-08-23 11:02:53 -0700

    Update songs.py

--- JESongBot/plugins/songs.py
+++ JESongBot/plugins/songs.py
@@ -48,7 +48,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("📥 Downloading the song by @UvinduBro 📥")
+    m.edit("📥 Downloading the song by @NM_rider 📥")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
